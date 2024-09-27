344b5d2fbd5b06230ada281381843ab42e7bc5bd
Florian <hi@florianpanchout.com>
2024-09-27 15:10:47 +0200

    feat: remove enverythong on homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,11 @@
 import Header from '@/components/layout/Header'
-import Actions from './_components/Actions'
-import Amis from './_components/Amis'
-import Contributions from './_components/Contributions'
-import Explanations from './_components/Explanations'
 import Heading from './_components/Heading'
-import Organisations from './_components/Organisations'
 
 export default async function Homepage() {
   return (
     <>
       <Header />
       <Heading />
-      <div className="mx-auto mb-12 flex w-full max-w-5xl flex-col flex-wrap items-center gap-12 px-4 md:mb-20 md:flex-row md:items-start md:px-8 lg:gap-28">
-        <Amis />
-        <Actions />
-      </div>
-      <Organisations />
-      <Explanations />
-      <Contributions />
     </>
   )
 }
