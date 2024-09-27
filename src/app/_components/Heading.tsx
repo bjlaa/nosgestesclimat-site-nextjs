08421a46f10b53991d78aaf9771c8c21f777ac68
Florian <hi@florianpanchout.com>
2024-09-27 15:06:10 +0200

    feat: remove icons

--- src/app/_components/Heading.tsx
+++ src/app/_components/Heading.tsx
@@ -1,13 +1,11 @@
 import Trans from '@/components/translation/Trans'
 import Buttons from './heading/Buttons'
-import Icons from './heading/Icons'
 import Partners from './heading/Partners'
 
 export default async function Heading() {
   return (
     <>
       <div className="relative flex h-[34rem] items-center justify-center overflow-hidden bg-gray-100 p-4 md:h-[36rem]">
-        <Icons />
         <div className="relative mb-2 max-w-sm text-center md:mb-0 md:max-w-2xl">
           <h1 className="md:text-5xl">
             <Trans>Connaissez-vous votre empreinte sur le climat ?</Trans>
