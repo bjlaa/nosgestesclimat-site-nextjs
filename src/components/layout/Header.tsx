f1ab8c10d14c9839f2c0e76dbf1621217a16338d
Florian <hi@florianpanchout.com>
2024-09-30 15:49:39 +0200

    feat: add margin for header

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -9,7 +9,7 @@ export default function Header({ isSticky = true }: Props) {
   return (
     <>
       {/* Displayed only on mobile (screens < 768px) */}
-
+      <div className="h-24 lg:hidden"></div>
       {/* Displayed only on desktop */}
       <HeaderDesktop isSticky={isSticky} />
     </>
