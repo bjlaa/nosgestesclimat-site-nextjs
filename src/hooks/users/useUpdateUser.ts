6d2e56e42bf5e19001a4477feae19c32e195579d
Paul Souche <paul@bettercallpaul.fr>
2025-01-13 10:13:32 +0100

    :recycle: update user brevo contact

---
+++ src/hooks/users/useUpdateUser.ts
@@ -0,0 +1,35 @@
+import { useMutation } from '@tanstack/react-query'
+import axios from 'axios'
+import { USER_URL } from '../../constants/urls'
+
+type Props = {
+  email?: string
+  userId: string
+}
+
+type FuncProps = {
+  newsletterIds: Record<number, boolean>
+  email?: string
+  name?: string
+}
+
+export function useUpdateUser({ email, userId }: Props) {
+  return useMutation({
+    mutationKey: ['userContact', userId],
+    mutationFn: async ({ newsletterIds, email: emailParam, name }: FuncProps) =>
+      axios.put(`${USER_URL}/${userId}`, {
+        name,
+        email: email || emailParam,
+        listIds: Object.entries(newsletterIds).reduce(
+          (acc: number[], [id, subscribed]) => {
+            if (subscribed) {
+              acc.push(+id)
+            }
+
+            return acc
+          },
+          []
+        ),
+      }),
+  })
+}
